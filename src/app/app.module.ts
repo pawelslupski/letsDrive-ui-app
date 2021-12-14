d21baa1bae2fa347f703b1b2b0c77878933c6799
pawelslupski <pawelslupski.go@gmail.com>
2021-12-14 20:05:17 +0100

    [LetsDrive-002] - plain basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MainLayoutComponent} from './main-layout/main-layout.component';
+import {MaterialModule} from "./material/material.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        MatToolbarModule
+        MaterialModule
     ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+const MATERIAL_MODULES: never[] = [
+
+];
+
+@NgModule({
+  exports: [...MATERIAL_MODULES],
+  declarations: []
+})
+export class MaterialModule { }
