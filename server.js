da494ae02b6a62fc0fefa8156bbebc9a983dc0b5
pawelslupski <pawelslupski.go@gmail.com>
2021-12-19 11:38:09 +0100

    [LetsDrive-003] - heroku initial config

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/letsDrive-app'));
+
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/letsDrive-app/'})
+);
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
